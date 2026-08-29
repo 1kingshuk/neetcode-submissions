9d0f677c0658ab6eaca605fd1c2ef954a831d2f0
1kingshuk <42107991+1kingshuk@users.noreply.github.com>
2026-08-29 23:53:31 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        for (int i=0; i<temperatures.length; i++) {
+            int count = 0;
+            boolean isGreater = false;
+            for (int j=i+1; j<temperatures.length; j++) {
+                if (temperatures[j]>temperatures[i]) {
+                    count++;
+                    isGreater = true;
+                    break;
+                }
+                count++;
+            }
+            result[i]=isGreater ? count : 0;
+        }
+        return result;
+    }
+}
